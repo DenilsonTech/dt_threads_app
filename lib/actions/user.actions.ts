047f0731f0d9342a4200d1d0930983ea83f8cfdf
unknown <denilsondiniz341@gmail.com>
2024-02-11 20:01:35 +0200

    first commit

---
+++ lib/actions/user.actions.ts
@@ -0,0 +1,169 @@
+"use server"
+
+import { revalidatePath } from "next/cache";
+import User from "../models/user.model";
+import { connectToDB } from "../mongoose"
+import Thread from "../models/thread.model";
+import { FilterQuery, SortOrder } from "mongoose";
+
+interface Params {
+    userId: string,
+    username: string,
+    name: string,
+    bio: string,
+    image: string,
+    path: string,
+}
+
+export async function updateUser({
+    userId,
+    username,
+    name,
+    bio,
+    image,
+    path,
+}: Params): Promise<void> {
+    connectToDB();
+
+    try {
+        await User.findOneAndUpdate(
+            {id: userId},
+            {
+                username: username.toLowerCase(),
+                name,
+                bio,
+                image,
+                onboarded: true,
+            },
+            {upsert: true},
+        );
+    
+        if(path === '/profile/edit') {
+            revalidatePath(path);
+        }
+    } catch (error: any) {
+        throw new Error(`Failed to create/update user: ${error.message}`);
+        
+    }
+}
+
+export async function fetchUser(userId: string) {
+    try {
+        connectToDB();
+
+        return await User
+            .findOne({ id: userId })
+          //  .populate({
+           //     path: 'communoties',
+           //     model: Community
+            //})
+    } catch (error: any) {
+        throw new Error(`Failed to create/update user: ${error.message}`);        
+    }
+}
+
+export async function fecthUserPosts (userId: string) {
+    try {
+        connectToDB();
+
+        //encontrar todos threads criados pelo user o userId dado
+
+        // TODO: populate Community
+        const threads = await User.findOne({ id: userId })
+            .populate({
+                path: 'threads',
+                model: Thread,
+                populate: {
+                    path: 'children',
+                    model: Thread,
+                    populate: {
+                        path: 'author',
+                        model: User,
+                        select: 'name image id'
+                    }
+                }
+            })
+
+            return threads;
+    } catch (error: any) {
+        throw new Error(`Failed to create/update user: ${error.message}`);
+    }
+}
+
+export async function fetchUsers({
+    userId,
+    searchString = "",
+    pageNumber =1,
+    pageSize = 20,
+    sortBy = "desc"
+ }: {
+    userId: string;
+    searchString?: string;
+    pageNumber?: number;
+    pageSize?: number;
+    sortBy?: SortOrder;
+ }){
+    try {
+        connectToDB();
+
+        const skipAmount = (pageNumber -1) * pageSize;
+
+        const regex =  new RegExp(searchString, "i");
+
+        const query: FilterQuery<typeof User> = {
+            id: { $ne: userId }
+        }
+
+        if(searchString.trim() !== ''){
+            query.$or =[
+                { username: { $regex: regex } },
+                { name: { $regex: regex } }
+            ]
+        }
+
+        const sortOptions  = { creadetAt: sortBy };
+        
+        const usersQuery = User.find(query)
+            .sort(sortOptions)
+            .skip(skipAmount)
+            .limit(pageSize);
+
+        const totalUsersCount = await User.countDocuments(query);
+
+        const users = await usersQuery.exec();
+
+        const isNext = totalUsersCount > skipAmount + users.length;
+
+        return { users, isNext };
+
+    } catch (error: any) {
+        throw new Error(`Failed to create/update user: ${error.message}`);
+    }
+}
+
+export async function getActivity(userId : string) {
+    try {
+        connectToDB();
+
+        // achar todo threads criados pelos usuarios
+        const userThreads = await Thread.find({ author: userId });
+
+        //Coletar todos os threads filhos ids (comentários) do campo dos filhos
+        const childThreads = userThreads.reduce((acc, userThread) => {
+            return acc.concat(userThread.children)
+        }, [])
+
+        const replies = await Thread.find({
+            _id: { $in: childThreads },
+            author: { $ne: userId }
+        }).populate({
+            path: 'author',
+            model: User,
+            select: 'name image _id'
+        })
+
+        return replies
+    } catch (error: any) {
+        throw new Error(`Failed to create/update user: ${error.message}`);
+    }
+}
